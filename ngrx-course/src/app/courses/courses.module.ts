1174bbab2ae6195d88168a8bd1368534e4e32ae8
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-06 21:56:28 -0300

    ngrx-course [ 05 - NgRx Entity In Depth ] 001 NgRx Entity - Section Kickoff, 002 NgRx Feature Design - Defining Actions First, 003 Loading NgRx Entity Data using a Router Resolver

--- ngrx-course/src/app/courses/courses.module.ts
+++ ngrx-course/src/app/courses/courses.module.ts
@@ -21,16 +21,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule, Routes} from '@angular/router';
-import { EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
+import {EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
 import {compareCourses, Course} from './model/course';
 
 import {compareLessons, Lesson} from './model/lesson';
+import {CoursesResolver} from "./courses.resolver";
 
 
 export const coursesRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    resolve: {
+      courses: CoursesResolver
+    }
 
   },
   {
@@ -73,7 +77,8 @@ export const coursesRoutes: Routes = [
     CourseComponent
   ],
   providers: [
-    CoursesHttpService
+    CoursesHttpService,
+    CoursesResolver
   ]
 })
 export class CoursesModule {
